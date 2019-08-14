16f5a0948db9951abf943e5ea2f38c99eddb23bb
ShimaneSuzuki <s163043@matsu.shimane-u.ac.jp>
2019-08-14 17:35:24 +0900

    progress

--- video.js
+++ video.js
@@ -4,72 +4,79 @@ const flipHorizontal = false;
 const stats = new Stats();
 const contentWidth = 270;
 const contentHeight = 480;
+var audio = new Audio('footstep.m4a');
+var end = false;
 
-bindPage();
-
-function playVideo() {
-  bindPage();
-	// //動画を再生
-	// v.play();
+function startEstimation() {
+  start();
 }
 
-async function bindPage() {
-    const net = await posenet.load(); // posenetの呼び出し
-    let video;
-    try {
-        video = await loadVideo(); // video属性をロード
-    } catch(e) {
-        console.error(e);
-        return;
-    }
-    detectPoseInRealTime(video, net);
+async function start() {
+  const net = await posenet.load(); // posenetの呼び出し
+  let video;
+  try {
+    video = await loadVideo(); // video属性をロード
+  } catch (e) {
+    console.error(e);
+    return;
+  }
+  detectPoseInRealTime(video, net);
 }
 
 // video属性のロード
 async function loadVideo() {
-    var video = document.getElementById('video')
-    video.play();
-    return video;
+  var video = document.getElementById('video')
+  video.play();
+  video.addEventListener("ended", function() {
+    end = true;
+  }, false);
+  return video;
 }
 
 // 取得したストリームをestimateSinglePose()に渡して姿勢予測を実行
 // requestAnimationFrameによってフレームを再描画し続ける
 function detectPoseInRealTime(video, net) {
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
-    const flipHorizontal = true; // since images are being fed from a webcam
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+  const flipHorizontal = true; // since images are being fed from a webcam
 
-    async function poseDetectionFrame() {
-        stats.begin();
-        let poses = [];
-        const pose = await net.estimateSinglePose(video, imageScaleFactor, flipHorizontal, outputStride);
-        poses.push(pose);
+  async function poseDetectionFrame() {
+    stats.begin();
+    let poses = [];
+    const pose = await net.estimateSinglePose(video, imageScaleFactor, flipHorizontal, outputStride);
+    poses.push(pose);
 
-        ctx.clearRect(0, 0, contentWidth,contentHeight);
+    ctx.clearRect(0, 0, contentWidth, contentHeight);
 
-        // ctx.save();
-        // ctx.scale(-1, 1);
-        // ctx.translate(-contentWidth, 0);
-        // ctx.drawImage(video, 0, 0, contentWidth, contentHeight);
-        // ctx.restore();
+    // ctx.save();
+    // ctx.scale(-1, 1);
+    // ctx.translate(-contentWidth, 0);
+    // ctx.drawImage(video, 0, 0, contentWidth, contentHeight);
+    // ctx.restore();
 
-        poses.forEach(({ score, keypoints }) => {
-            // keypoints[15]には左足、keypoints[16]には右足の予測結果が格納されている
-            drawWristPoint(keypoints[15],ctx);
-            drawWristPoint(keypoints[16],ctx);
-        });
+    poses.forEach(({
+      score,
+      keypoints
+    }) => {
+      // keypoints[15]には左足、keypoints[16]には右足の予測結果が格納されている
+      drawWristPoint(keypoints[15], ctx);
+      drawWristPoint(keypoints[16], ctx);
+    });
 
-        stats.end();
+    audio.play();
+    stats.end();
 
-        requestAnimationFrame(poseDetectionFrame);
+    if (!video.ended) {
+      requestAnimationFrame(poseDetectionFrame);
     }
-    poseDetectionFrame();
+  }
+  poseDetectionFrame();
 }
 
 // 与えられたKeypointをcanvasに描画する
-function drawWristPoint(wrist,ctx){
-    ctx.beginPath();
-    ctx.arc(wrist.position.x , wrist.position.y, 3, 0, 2 * Math.PI);
-    ctx.fillStyle = "pink";
-    ctx.fill();
+function drawWristPoint(wrist, ctx) {
+  ctx.beginPath();
+  ctx.arc(wrist.position.x, wrist.position.y, 3, 0, 2 * Math.PI);
+  ctx.fillStyle = "pink";
+  ctx.fill();
 }
